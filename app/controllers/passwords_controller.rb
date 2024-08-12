f0fb87e6807064062fb9b2db03a49a5bd4adeb32
Justin Navarro <info@justinnavarro.net>
2024-08-12 09:18:10 -0400

    add edit and update methods

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -23,6 +23,17 @@ class PasswordsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @password.update(password_params)
+      redirect_to @password
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def password_params
