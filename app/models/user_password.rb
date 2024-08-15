9d437b07c8602cf7aacf7b181d781b499a98bc64
Justin Navarro <info@justinnavarro.net>
2024-08-15 10:26:33 -0400

    update the user_password model with roles

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -1,4 +1,8 @@
 class UserPassword < ApplicationRecord
+  ROLES = %w{owner viewer editor}
+
   belongs_to :user
   belongs_to :password
+
+  validates :role, presence: true, inclusion: { in: ROLES }
 end
